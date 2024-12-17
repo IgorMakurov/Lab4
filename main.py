552d5e7cbb6f5dfebee8ddd77f78a666088a94ad
IgorMakurov <snegir2002@yandex.ru>
2024-12-17 13:51:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,29 @@
 
 import re
 
+digit_map = {
+        '-': 'минус', '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
+        '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
+    }
+
 five_digit_numbers = []
 
-with open('input.txt', 'r') as f:
-    char = f.read()
-    matches = re.findall(r'\b\d{5}\b', char)
+
+with open('input.txt', 'r') as file:
+    content = file.read()
+    matches = re.findall(r'-?\d{5}', content)
     five_digit_numbers = [int(match) for match in matches]
 
+print("Найденные числа:")
+for num in five_digit_numbers:
+    print(num, end=" ")
+
 count = len(five_digit_numbers)
-print(f"Количество чисел из 5 цифр: {count}")
+print(f"\nКоличество чисел из 5 цифр: {count}")
 
 if five_digit_numbers:
     min_number = min(five_digit_numbers)
     min_number_str = str(min_number)
     print("Минимальное число прописью:", end=" ")
-for digit in min_number_str:
-    print(digit, end=" ")
+    for digit in min_number_str:
+        print(digit + ' ' + digit_map[digit], end=" ")
